67cd7df2e959fe4b84492f86d8f49cda1cb6a2fa
Tomasz Niedziela <tomekniedz@gmail.com>
2015-09-14 07:53:27 +0200

    tests for demo done

--- src/java/test/TestDatasetObligation.java
+++ src/java/test/TestDatasetObligation.java
@@ -12,18 +12,22 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestDatasetDouble {
+public class TestDatasetObligation {
     public static void main(String[] args){
-        Path path = FileSystems.getDefault().getPath("D:\\Desktop\\Kompilatory\\dataset_analyzer\\src\\java\\test\\test_data\\test_reaction.txt");
+        Path path = FileSystems.getDefault().getPath("D:\\Desktop\\Kompilatory\\dataset_analyzer\\src\\java\\test\\test_data\\test_obligation.txt");
         String delimiter = ",";
         List<Field> fields = new ArrayList<>();
         Field f1 = new Field("f1", Field.Type.Number);
         Field f2 = new Field("f2", Field.Type.String);
-        Field f3 = new Field("f3", Field.Type.Time, "HH:mm:ss");
+        Field f3 = new Field("f3", Field.Type.Number);
+        Field f4 = new Field("f4", Field.Type.Number);
+        Field f5 = new Field("f5", Field.Type.Time, "HH:mm:ss");
 
         fields.add(f1);
         fields.add(f2);
         fields.add(f3);
+        fields.add(f4);
+        fields.add(f5);
 
         // Read data
         Reader reader = new Reader(path, fields, delimiter);
@@ -31,20 +35,24 @@ public class TestDatasetDouble {
         System.out.println(dataset.toString());
 
         // Define Event
-        Double limit = 100.00;
+        Double light = 90.00;
+        Double noise = 80.00;
         String weather = "pada";
 
-        Occurance o1 = new Occurance(Occurance.Condition.More, limit);
-        Occurance o2 = new Occurance(Occurance.Condition.Equal, weather);
+        Occurance o1 = new Occurance(Occurance.Condition.More, light);
+        Occurance o2 = new Occurance(Occurance.Condition.More, noise);
+        Occurance o3 = new Occurance(Occurance.Condition.Equal, weather);
 
         Event eventA = new Event();
-        eventA.addOccurance("f1", o1);
+        eventA.addOccurance("f3", o1);
+        eventA.addOccurance("f2", o3);
 
         Event eventB = new Event();
-        eventB.addOccurance("f2", o2);
+        eventB.addOccurance("f4", o2);
+        eventA.addOccurance("f2", o3);
 
         // Filter data
-        DoubleEventExpression.Operator op = DoubleEventExpression.Operator.Reaction;
+        DoubleEventExpression.Operator op = DoubleEventExpression.Operator.Obligation;
         DoubleEventExpression ex = new DoubleEventExpression(eventA, eventB, op);
 
         DatasetFilter filter = new DatasetFilter(ex);
---
+++ src/java/test/TestDatasetResponse.java
@@ -0,0 +1,64 @@
+/*
+Author: Tomasz Niedziela-Brach
+ */
+package test;
+
+import dataset.*;
+import reader.Field;
+import reader.Reader;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestDatasetResponse {
+    public static void main(String[] args){
+        Path path = FileSystems.getDefault().getPath("D:\\Desktop\\Kompilatory\\dataset_analyzer\\src\\java\\test\\test_data\\test_response.txt");
+        String delimiter = ",";
+        List<Field> fields = new ArrayList<>();
+        Field f1 = new Field("f1", Field.Type.Number);
+        Field f2 = new Field("f2", Field.Type.String);
+        Field f3 = new Field("f3", Field.Type.Number);
+        Field f4 = new Field("f4", Field.Type.Number);
+        Field f5 = new Field("f5", Field.Type.Time, "HH:mm:ss");
+
+        fields.add(f1);
+        fields.add(f2);
+        fields.add(f3);
+        fields.add(f4);
+        fields.add(f5);
+
+        // Read data
+        Reader reader = new Reader(path, fields, delimiter);
+        Dataset dataset = reader.GenerateDataset();
+        System.out.println(dataset.toString());
+
+        // Define Event
+        Double light = 90.00;
+        Double noise = 80.00;
+        String weather = "pada";
+
+        Occurance o1 = new Occurance(Occurance.Condition.More, light);
+        Occurance o2 = new Occurance(Occurance.Condition.More, noise);
+        Occurance o3 = new Occurance(Occurance.Condition.Equal, weather);
+
+        Event eventA = new Event();
+        eventA.addOccurance("f3", o1);
+        eventA.addOccurance("f2", o3);
+
+        Event eventB = new Event();
+        eventB.addOccurance("f4", o2);
+        eventA.addOccurance("f2", o3);
+
+        // Filter data
+        DoubleEventExpression.Operator op = DoubleEventExpression.Operator.Response;
+        DoubleEventExpression ex = new DoubleEventExpression(eventA, eventB, op);
+
+        DatasetFilter filter = new DatasetFilter(ex);
+        Dataset result = filter.FilterDataset(dataset);
+
+        System.out.println(result.toString());
+        System.out.println("Result of matching logical operator: " + ex.VerifyOperator());
+    }
+}
